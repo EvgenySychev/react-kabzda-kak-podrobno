3e7be18853dd2a72cf0c87e2d78e81fbf4ec0d3a
EvgenySychev <samara808@yandex.ru>
2022-06-19 11:59:20 +0400

    22 lesson (whit Clock stories)

---
+++ src/components/Clock/Clock.stories.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import {Clock} from "./Clock";
+
+
+export default {
+    title: 'Clock',
+    component: Clock,
+};
+
+export const ClockOn = () => <Clock/>
---
+++ src/components/Clock/Clock.tsx
@@ -0,0 +1,31 @@
+import React, {useEffect} from "react";
+import {useState} from "react";
+
+export const Clock = () => {
+    const [seconds, setSeconds] = useState(55)
+    const [minutes, setMinutes] = useState(59)
+    const [hours, setHours] = useState(0)
+
+
+    useEffect(() => {
+        setInterval(() => {
+            setSeconds(s => s + 1)
+        }, 1000)
+    }, [])
+
+    if (seconds===60) {
+        setSeconds(0)
+        setMinutes(minutes + 1)
+    }
+
+    if (minutes === 60) {
+        setSeconds(0)
+        setMinutes(0)
+        setHours(hours+1)
+    }
+
+    return (
+        <div>
+            {`${hours}:${minutes}:${seconds}`}
+        </div>)
+}
